5e2419f8895eb0f5e6969d2590a9bbf09de0c756
Armando Martinez Mandujano <armando@muuklabs.com>
2018-10-12 18:39:06 -0500

    Trying nodejs

--- calculator.js
+++ calculator.js
@@ -2,16 +2,14 @@ var readline = require('readline');
 var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  terminal: true
+  terminal: false
 });
 
 var arr = [];
-var i = 0;
+
 rl.on('line', function(line){
   arr.push(parseInt(line));
-  i++;
-  if(i == 2){
-    var result = arr[0] + arr[1];
-    console.log(result);
-  }
 });
+
+var result = arr[0] + arr[1];
+console.log(result);
