68a880ac81b2e591f7061ad19aab9c4d91050f6b
Armando Martinez Mandujano <armando@muuklabs.com>
2018-10-12 19:11:21 -0500

    This is the good one

--- calculator.js
+++ calculator.js
@@ -6,13 +6,19 @@ var rl = readline.createInterface({
 });
 
 var arr = [];
+var i = 0;
+var result = 0;
 
-rl.on('line', function(line){
-  arr.push(parseInt(line));
-  // console.log(line);
-  // console.log(typeof(line));
+// rl.on('line', function(line){
+//   // console.log(parseInt(line));
+//   arr[i++] = parseInt(line);
+//   console.log();
+// });
 
-
-  var result = arr[0] + arr[1];
-  console.log(result);
+rl.on('line', (line) => {
+  arr[i++] = parseInt(line);
+}).on('close', () => {
+  var res = arr[0] + arr[1];
+  console.log(res);
+  process.exit(0);
 });
